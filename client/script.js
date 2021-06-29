2e472ea9793fcee59223a4410adb7731bfa95b18
akashkhambay <akashkhambay@hotmail.co.uk>
2021-06-29 20:47:55 +0100

    Changed google colors to match the google colors actually

--- client/script.js
+++ client/script.js
@@ -1,1 +1,12 @@
 // Event listener stuff
+const submitBtn = document.getElementsByClassName('buttons');
+
+// Adding the event listener for when we click the submit button something happens, preferably the data from the food file
+
+submitBtn.addEventListener('click', getFood);
+
+
+function getFood(){
+    fetch('http://localhost:3000/foods')
+    .then(r => r.json())
+}
