914a09bc49e05aeea45cea1e9914fefa7ef40559
akashkhambay <akashkhambay@hotmail.co.uk>
2021-06-29 20:12:00 +0100

    Adding fetch to client side to help get the data

--- server/app.js
+++ server/app.js
@@ -14,3 +14,14 @@ app.use(cors());
 app.get('/', (req,res) => res.send('Hello Foods!'))
 
 app.get('/foods' , (req, res) => res.send(foodList));
+
+// const action = async () => {
+//     const response = await fetch ('http://localhost3000/foods.json');
+//     const myJson = await response.json();
+// }
+
+// function getAllFoods(){
+//     fetch('http://localhost:3000/foods')
+//     .then(r => r.json())
+//     .then
+}
